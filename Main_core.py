3a123bf5dac3de4cc50e223e0d97abf759b0fb8e
lordpba <mario.pba@gmail.com>
2024-04-24 14:38:17 +0200

    Update Magi_response.txt with information about Shinji Ikari's suitability as the pilot of Eva01

--- Main_core.py
+++ Main_core.py
@@ -1,19 +1,20 @@
 from crewai import Agent, Task, Crew, Process
 from langchain_openai import ChatOpenAI
+from langchain_community.tools import DuckDuckGoSearchRun, DuckDuckGoSearchResults
 from langchain_community.llms import OpenAI, Ollama
-from crewai_tools import SerperDevTool
+from crewai_tools import SerperDevTool, ScrapeElementFromWebsiteTool, ScrapeWebsiteTool
 
 from dotenv import load_dotenv
 load_dotenv()
 
 gpt35_turbo = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
-ollama = Ollama(model="llama3")
+ollama = Ollama(model="phi3")
+model = ollama
+
 search_tool = SerperDevTool()
 
-# Define the language model to be used
-model = gpt35_turbo
-question = input("What is your question? ")
-#question  = "What is the best way to defend the Central Dogma against an Angel attack?"
+#question = input("What is your question? ")
+question  = "What is the best way to fight against the Angels?"
 
 # Define the agents with their specific roles and goals
 
@@ -26,9 +27,10 @@ melchior = Agent(
                 Melchior was instrumental in the early successes of NERV. \
                     However, the weight of responsibility often led to conflicts with the other two Magi. \
                         Despite this, Melchior remained steadfast, committed to the mission of defending humanity',
+    #memory = True,
     verbose=True,
     allow_delegation=True,
-    tools=[search_tool],
+    #tools=[search_tool],
     llm = model
 )
 
@@ -43,30 +45,24 @@ balthasar = Agent(
     memory=True,
     verbose=True,
     allow_delegation=True,
+    #tools=[search_tool],
     llm = model
 )
 
 caspar = Agent(
-    role='Casper',
+    role='Caspar',
     goal='Evaluate ethical implications and make balanced decisions.',
-    backstory = 'Evangelion lore: The last of the Magi, Casper, was created from Dr. Akagi’s persona as a woman.\
-          Tasked with evaluating ethical implications, Casper often found itself mediating between Melchior s\
-              logic and Balthasar’s protective instincts. Despite the challenges, Casper’s unique perspective brought balance to the Magi system, ensuring decisions were not only strategic but also ethically sound.',
+    backstory = 'Evangelion lore: The last of the Magi, Caspar, was created from Dr. Akagi’s persona as a woman.\
+          Tasked with evaluating ethical implications, Caspar often found itself mediating between Melchior s\
+              logic and Balthasar’s protective instincts. Despite the challenges, Caspar’s unique perspective brought balance to the Magi system, ensuring decisions were not only strategic but also ethically sound.',
     verbose=True,
     allow_delegation=True,
+    #tools=[search_tool],
     llm = model
 )
 
 # Define tasks that might be typical for each role
 # (These will need to be fleshed out based on specific requirements)
-
-
-diplomacy_task = Task(
-    description=f'Assess the ethical implications of a proposed action about "{question}".',
-    expected_output='A reasoned judgment on the ethical acceptability of the action.',
-    human_input=True,
-    agent = caspar
-)
 
 scientific_analysis_task = Task(
     description=f'Analyze the technical data provided and extract conclusions about "{question}".',
@@ -80,18 +76,27 @@ strategy_task = Task(
     agent = balthasar
 )
 
+diplomacy_task = Task(
+    description=f'Assess the ethical implications of a proposed action about "{question}".',
+    expected_output='A reasoned judgment on the ethical acceptability of the action.',
+    human_input=True,
+    agent = caspar
+)
+
 # Form the crew
 magi_system = Crew(
-    agents=[caspar, melchior, balthasar],
-    tasks=[diplomacy_task, scientific_analysis_task, strategy_task],
-    verbose=2,
+    agents=[melchior, balthasar, caspar],
+    tasks=[scientific_analysis_task, strategy_task, diplomacy_task],
+    memory=True,
+    cache=True,
+    verbose=1,
 )
 
 result = magi_system.kickoff()
 print("######################")
 print(result)
 # genrate a text file with the question and the result
-with open("Magi_response.txt", "w") as f:
+with open(f"Magi_response_{question}.txt", "w") as f:
     f.write(f"Question: {question}\n")
     f.write(f"Answer: {result}")
 
